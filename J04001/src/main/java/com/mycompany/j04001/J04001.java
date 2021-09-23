c3fac43b6a3d640e1cb984263d14cc17f3580abc
NGUYEN TIEN DUNG <dungtpa2@gmail.com>
2021-09-23 21:32:10 +0700

    Wrong Answer

---
+++ J04001/src/main/java/com/mycompany/j04001/J04001.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.j04001;
+
+/**
+ *
+ * @author Admin
+ */
+public class J04001 {
+    private double x,y;
+    public J04001(){
+    }
+    public J04001(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    // hàm tạo sao chép.
+    public J04001(J04001 p){
+        this.x = p.x;
+        this.y = p.y;
+    }
+    public double getX(){
+        return x;
+    }
+    public void setX(double x){
+        this.x = x;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setY(double y){
+        this.y = y;
+    }
+    // taoj hamf sao chep secondpoint
+    public double distance(J04001 secondPoint){
+        double d1 = this.x - secondPoint.x;
+        double d2 = this.y - secondPoint.y;
+        double dis = Math.sqrt(d1*d1+d2*d2);
+        return dis;
+    }
+    public double distance(J04001 p1, J04001 p2){
+        double d1 = p1.x - p2.x;
+        double d2 = p1.y - p2.y;
+        double dis = Math.sqrt(d1*d1 + d2*d2);
+        return dis;
+    }
+}
---
+++ J04001/src/main/java/com/mycompany/j04001/Nhap.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.j04001;
+
+/**
+ *
+ * @author Admin
+ */
+import java.util.*;
+public class Nhap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            J04001 P1 = new J04001();
+            J04001 P2 = new J04001();
+            P1.setX(sc.nextDouble());
+            P1.setY(sc.nextDouble());
+            P2.setX(sc.nextDouble());
+            P2.setY(sc.nextDouble());
+            double res = P1.distance(P2);
+            String s = String.format("%.4f",res);
+            System.out.println(s);
+        }
+    }
+}
