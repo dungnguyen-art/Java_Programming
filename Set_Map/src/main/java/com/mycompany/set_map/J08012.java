b673bbce7395c67480f6e08e06fb6dd66a82abc4
NGUYEN TIEN DUNG <dungtpa2@gmail.com>
2021-10-03 15:58:46 +0700

    AC

--- Set_Map/src/main/java/com/mycompany/set_map/J08012.java
+++ Set_Map/src/main/java/com/mycompany/set_map/J08012.java
@@ -9,25 +9,40 @@ package com.mycompany.set_map;
  *
  * @author Admin
  */
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Arrays;
 public class J08012 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashSet<Integer> hs_v = new HashSet<>();
-        HashSet<Integer> hs_e = new HashSet<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
         int n = sc.nextInt();
         int tmp = n;
         while (n-->1){
-            int[] arr = new int[2];
-            arr[0] = sc.nextInt();
-            arr[1] = sc.nextInt();
-            Arrays.sort(arr);
-            hs_v.add(arr[0]);
-            hs_e.add(arr[1]);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(!hm.containsKey(a)){
+                hm.put(a, 1);
+            }
+            else{
+                hm.put(a, hm.get(a)+1);
+            }
+            if(!hm.containsKey(b)){
+                hm.put(b, 1);
+            }
+            else{
+                hm.put(b, hm.get(b)+1);
+            }
         }
-        if(hs_v.size() == 1 && hs_e.size() == tmp-1){
+        int cnt_0 = 0,cnt_1 = 0;
+        for (Integer i: hm.keySet()){
+            if(hm.get(i) == 1){
+                cnt_0 += 1;
+            }
+            else{
+                cnt_1 += 1;
+            }
+        }
+        if (cnt_0 + cnt_1 == tmp){
             System.out.println("Yes");
         }
         else{
